f71ba33e0b63e770bc83f541728e338f53e1ccd8
Aldiansyah <aldiansyahunip@gmail.com>
2022-08-20 07:14:32 +0700

    initial

---
+++ 12-http.js
@@ -0,0 +1,20 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Welcome to our home page')
+    }
+    if (req.url === '/about') {
+        res.end('Here is out short history')
+    }
+
+    res.end(`
+
+        <h1>Oops!!</h1>
+        <p>We can't seem to</p>
+        <a href="/">Back to Home</a>
+    
+    `)
+})
+
+server.listen(5000)
